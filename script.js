79a93701d6e97504fb7a92e656a486f0d39fd17f
Francisco <80384593+Francisco2109@users.noreply.github.com>
2025-01-22 10:35:10 -0300

    dynamicaaaaaa

--- script.js
+++ script.js
@@ -7,7 +7,46 @@ let compras = [];
 let precioAcumulado = 0;
 let contadorProductos = 0;
 
-eventos();
+const productosLista = [
+    {id: 0, img: "img/productos/generico.png", title: "teclado gamer1", precio: 1800},
+    {id: 1, img: "img/productos/generico.png", title: "teclado gamer2", precio: 3000},
+    {id: 2, img: "img/productos/generico.png", title: "teclado gamer3", precio: 5800},
+    {id: 3, img: "img/productos/generico.png", title: "teclado gamer4", precio: 4800},
+    {id: 4, img: "img/productos/generico.png", title: "teclado gamer5", precio: 7800},
+    {id: 5, img: "img/productos/generico.png", title: "teclado gamer6", precio: 9800},
+    {id: 6, img: "img/productos/generico.png", title: "teclado gamer7", precio: 800},
+    {id: 7, img: "img/productos/generico.png", title: "teclado gamer8", precio: 2800},
+    {id: 8, img: "img/productos/generico.png", title: "teclado gamer9", precio: 6800},
+    {id: 9, img: "img/productos/generico.png", title: "teclado gamer10", precio: 10800}
+]
+
+// Carga de Datos
+function cargarProductos() {
+    let indice = 0;
+    productosLista.forEach(producto => {
+        if (indice < productosLista.length) {
+                let item = document.createElement("div");
+                item.innerHTML = `
+                    <div class="cards">
+                        <div>
+                            <img src="${producto.img}" class="producto-img">
+                        </div>
+                        <h5 class="producto-title">${producto.title}</h5>
+                        <p>$<span class="producto-precio">${producto.precio}</span></p>
+                        <a href="" producto-id="${producto.id}" class="btn-comprar">Agregar al Carrito</a>
+                    </div>
+                `;
+
+                containerProductos.appendChild(item);
+                indice ++
+            }
+        }
+    )
+}
+cargarProductos()
+
+
+// Eventos y Manejo Carrito 
 cargarLocalStorage();
 
 function eventos(){
@@ -47,12 +86,12 @@ function removerProducto(e) {
     guardarLocalStorage();
 }
 
-function leerDatosHtml(producto) {
+function leerDatosHtml(productoSeleccionado) {
     const infoProducto = {
-        img: producto.querySelector(".producto-img").src,
-        title: producto.querySelector(".title").textContent,
-        precio: producto.querySelector(".producto-precio").textContent,
-        id: producto.querySelector("a").getAttribute("producto-id"),
+        img: productoSeleccionado.querySelector(".producto-img").src,
+        title: productoSeleccionado.querySelector(".producto-title").textContent,
+        precio: productoSeleccionado.querySelector(".producto-precio").textContent,
+        id: productoSeleccionado.querySelector("a").getAttribute("producto-id"),
         cant: 1 
     }
 
@@ -153,3 +192,5 @@ function abrirCarrito(){
 function bienvenido(){
     alert("Bienvenido! Tenemos Ofertas en Fernet para el este caluroso Finde!")
 }
+
+eventos();
